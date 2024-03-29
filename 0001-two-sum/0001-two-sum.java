b733af1ea4bb6d3cb7e9be4252a17d18b44c8f23
DFDRDODM95 <130643312+DFDRDODM95@users.noreply.github.com>
2024-03-29 10:54:55 +0900

    Time: 1598 ms (5.08%), Space: 45.6 MB (9.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,23 +1,28 @@
 class Solution {
-        public int[] twoSum(int[] nums, int target) {
-            return backtracking(nums, target, 0, new ArrayList<Integer>());
-        }
+    public int[] twoSum(int[] nums, int target) {
+    return backtracking(nums, target, 0, new ArrayList<Integer>(), 2);
+  }
 
-        private int[] backtracking(int[] nums, int target, int start, List<Integer> ans) {
-            if (ans.size() == 2) {
-                if (nums[ans.get(0)] + nums[ans.get(1)] == target) {
-                    return new int[]{ans.get(0), ans.get(1)};
-                }
-                return null;
-            }
-            for (int i = start; i < nums.length; i++) {
-                ans.add(i);
-                int[] result = backtracking(nums, target, i + 1, ans);
-                if (result != null) {
-                    return result;
-                }
-                ans.remove(ans.size() - 1);
-            }
-            return null;
-        }
+  private int[] backtracking(int[] nums, int target, int start, List<Integer> ans, int k) {
+    if (ans.size() == k) {
+      int sum = 0;
+      for(int idx : ans) {
+        sum += nums[idx];
+      }
+      if (sum == target) {
+        return ans.stream().mapToInt(i -> i).toArray();
+      }
+      return null;
+    }
+
+    for (int i = start; i < nums.length; i++) {
+      ans.add(i);
+      int[] result = backtracking(nums, target, i + 1, ans, k);
+      if (result != null) {
+        return result;
+      }
+      ans.remove(ans.size() - 1);
+    }
+    return null;
+  }
 }
