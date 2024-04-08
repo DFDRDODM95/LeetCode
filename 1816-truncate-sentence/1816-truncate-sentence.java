c0f24573d0e5f6e73cb5fc2f31a5e487294d61ea
DFDRDODM95 <130643312+DFDRDODM95@users.noreply.github.com>
2024-04-08 22:37:23 +0900

    Time: 1 ms (75.28%), Space: 41.8 MB (56.61%) - LeetHub

---
+++ 1816-truncate-sentence/1816-truncate-sentence.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < k && i < words.length; i++) {
+            result.append(words[i]).append(" ");
+        }
+        return result.toString().trim();
+    }
+}
